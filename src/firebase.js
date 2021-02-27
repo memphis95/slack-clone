ebabce209cd1b67c04b5fd247a492d3a43d8229b
Ankit Goel <goelankit1995@gmail.com>
2021-02-27 12:00:33 +0530

    New project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3vescPJvECrMC-rbxsCL_7frNtt4CXYY",
+    authDomain: "slack-clone-24cd3.firebaseapp.com",
+    projectId: "slack-clone-24cd3",
+    storageBucket: "slack-clone-24cd3.appspot.com",
+    messagingSenderId: "870633472996",
+    appId: "1:870633472996:web:29ec04cca8b29bc4aefdf1"
+  };
+  
